b947c662063521d1dea8681b457842809a27b322
sharma151 <sharmasaurav150503@gmail.com>
2025-01-21 10:12:58 +0545

    qr code updated

---
+++ src/Pages/Businesscard.jsx
@@ -0,0 +1,24 @@
+import Form from "../Components/Form";
+import Template1 from "../Templates/Template1";
+import Template2 from "../Templates/Template2";
+import { UserDataContext } from "../Context/userdatacontext";
+import { useContext } from "react";
+import "../style/Pages/Businesscard.scss";
+const Businesscard = () => {
+  const { userData } = useContext(UserDataContext);
+
+  return (
+    <>
+      <div className="maindiv">
+        <div className="cards">
+          <Template1 temp1Data={userData} />
+          <Template2 temp2Data={userData} />
+        </div>
+
+        <Form className="form" />
+      </div>
+    </>
+  );
+};
+
+export default Businesscard;
