8e99d16a8784b03eca0af42bf3d195d39458dca7
sharma151 <sharmasaurav150503@gmail.com>
2025-01-22 17:12:59 +0545

    download feature added

--- src/Pages/Qrcode.jsx
+++ src/Pages/Qrcode.jsx
@@ -1,10 +1,12 @@
 import "../style/Pages/Qrcode.scss";
 import QRCode from "react-qr-code";
 import { useState } from "react";
-
+import { useRef } from "react";
+import DownloadButton from "../Components/DownloadButton";
 const Qrcode = () => {
   const [inputValue, setInputValue] = useState("");
   const [qrValue, setQrValue] = useState("");
+  const cardRef = useRef(null);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -16,22 +18,34 @@ const Qrcode = () => {
 
   return (
     <div className="qr-code-generator">
-      <h1>QR Code Generator</h1>
-      <input
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Enter text, number, or paragraph..."
-        rows="4"
-        className="input-textarea"
-      />
-      <button onClick={handleGenerate} className="generate-button">
-        Generate QR Code
-      </button>
-      {qrValue && (
-        <div className="qr-code-display">
-          <QRCode value={qrValue} className="qrCode" />
-        </div>
-      )}
+      <div className="container">
+        <h1>QR Code Generator</h1>
+        <p className="description">
+          Create your personalized QR code in seconds.
+        </p>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter your information here..."
+          className="input-field"
+        />
+        <button onClick={handleGenerate} className="generate-button">
+          Generate QR Code
+        </button>
+        {qrValue && (
+          <div className="qr-code-display " ref={cardRef}>
+            <QRCode value={qrValue} size={150} className="qrCode" />
+            <div className="Qrcode-dbtn">
+              <DownloadButton
+                targetRef={cardRef}
+                fileName="Qrcode"
+                className="qrcodestyle"
+              />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
