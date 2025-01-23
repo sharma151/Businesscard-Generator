6282f3d0ed53c6babf6cfab2c00393b5b2cf55a6
sharma151 <sharmasaurav150503@gmail.com>
2025-01-23 13:40:44 +0545

    vcard added

--- src/Context/userdatacontext.jsx
+++ src/Context/userdatacontext.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState, createContext } from "react";
-import QRCode from "react-qr-code";
+// import QRCode from "react-qr-code";
 // eslint-disable-next-line react-refresh/only-export-components
 export const UserDataContext = createContext();
 
@@ -16,21 +16,24 @@ export const UserDataProvider = ({ children }) => {
     email: "",
   });
 
-  const [qrCode, setQrCode] = useState("");
+  const vCardData = [
+    "BEGIN:VCARD",
+    "VERSION:4.0",
+    `FN:${userData?.fname || "First Name"} ${userData?.lname || "Last Name"}`,
+    `ORG:${userData?.company || "Company"}`,
+    `TITLE:${userData?.designation || "Designation"}`,
+    `EMAIL:${userData?.email || "username@example.com"}`,
+    `TEL;TYPE=work,voice:${
+      userData?.countryCode && userData?.phoneNumber
+        ? `+${userData.countryCode}${userData.phoneNumber}`
+        : "+939876543230"
+    }`,
+    `URL:${userData?.website || "https://www.example.com"}`,
+    "END:VCARD",
+  ].join("\n");
 
-  // Function to generate QR Code
-  const generateQrCode = (data) => {
-    QRCode.toDataURL(data, (err, url) => {
-      if (err) console.error(err);
-      setQrCode(url);
-    });
-  };
-
-  console.log("context userdata here", qrCode);
   return (
-    <UserDataContext.Provider
-      value={{ userData, setUserData, qrCode, generateQrCode }}
-    >
+    <UserDataContext.Provider value={{ userData, setUserData, vCardData }}>
       {children}
     </UserDataContext.Provider>
   );
