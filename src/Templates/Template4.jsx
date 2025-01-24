9d45022422bcf209e30e9a871c34bd69adfce704
sharma151 <sharmasaurav150503@gmail.com>
2025-01-24 11:19:37 +0545

    logo added to template

--- src/Templates/Template4.jsx
+++ src/Templates/Template4.jsx
@@ -3,6 +3,10 @@ import "../style/Templates/Template4.scss";
 import { UserDataContext } from "../Context/userdatacontext";
 import { useContext } from "react";
 import QRCode from "react-qr-code";
+import { FaBattleNet } from "react-icons/fa6";
+import { HiOutlineMail } from "react-icons/hi";
+import { HiPhone, HiMiniGlobeAsiaAustralia } from "react-icons/hi2";
+import { CgWebsite } from "react-icons/cg";
 const Template4 = ({ temp4Data }) => {
   const { vCardData } = useContext(UserDataContext);
   return (
@@ -10,18 +14,36 @@ const Template4 = ({ temp4Data }) => {
       <div className="template4">
         <div className="card-left">
           <div className="qr-code">
-            <QRCode value={vCardData} className="temp4qrcode"/>
+            <QRCode value={vCardData} className="temp4qrcode" />
           </div>
-          <ul className="contact-info">
-            <li>{temp4Data.website || "www.example.com"}</li>
-            <li>
-              {" "}
+          <div className="temp4-details">
+            <div className="temp4-h">
+              <HiOutlineMail />
+              &nbsp;
+              {temp4Data?.email || "username@example.com"}
+            </div>
+            <div className="temp4-h">
+              <HiPhone />
+              &nbsp;
               {temp4Data?.countryCode && temp4Data?.phoneNumber
                 ? `+${temp4Data.countryCode}-${temp4Data.phoneNumber}`
-                : "+977-98xxxxxx98"}
-            </li>
-            <li> {temp4Data?.email || "username@example.com"}</li>
-          </ul>
+                : "+94-98xxxxxx98"}
+            </div>
+            {temp4Data?.website && (
+              <div className="temp4-h">
+                <HiMiniGlobeAsiaAustralia />
+                &nbsp;
+                {temp4Data.website || "www.example.com"}
+              </div>
+            )}
+            {temp4Data?.gst && (
+              <div className="temp4-h">
+                <CgWebsite />
+                &nbsp;
+                {temp4Data.gst || "24AAACC4206D4ZT"}
+              </div>
+            )}
+          </div>
         </div>
         <div className="card-right">
           <div className="name">
@@ -33,7 +55,7 @@ const Template4 = ({ temp4Data }) => {
             <br />
             <small>{temp4Data?.designation || "Designation"}</small>
           </div>
-          <div className="logo">A</div>
+          <FaBattleNet  className="logo"/>
         </div>
       </div>
     </>
