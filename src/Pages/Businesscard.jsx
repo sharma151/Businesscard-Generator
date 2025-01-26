aacf82990fb65325c963e3738ec7d4a0d5b22116
sharma151 <sharmasaurav150503@gmail.com>
2025-01-26 18:32:53 +0545

    new templates added

--- src/Pages/Businesscard.jsx
+++ src/Pages/Businesscard.jsx
@@ -1,9 +1,12 @@
 import Form from "../Components/Form";
 import Template1 from "../Templates/Template1";
 import Template2 from "../Templates/Template2";
+import Template3 from "../Templates/Template3";
 import { UserDataContext } from "../Context/userdatacontext";
 import { useContext } from "react";
 import "../style/Pages/Businesscard.scss";
+import Template4 from "../Templates/Template4";
+import Template5 from "../Templates/Template5";
 const Businesscard = () => {
   const { userData } = useContext(UserDataContext);
 
@@ -13,6 +16,9 @@ const Businesscard = () => {
         <div className="cards">
           <Template1 temp1Data={userData} />
           <Template2 temp2Data={userData} />
+          <Template3 temp3Data={userData} />
+          <Template4 temp4Data={userData} />
+          <Template5 temp5Data={userData} />
         </div>
         <div className="form">
           <Form />
