6dbc7b5062f182dc78232403a04f80f1c0fb49c7
sharma151 <sharmasaurav150503@gmail.com>
2025-06-29 22:22:35 +0545

    fix: template 1 refactored with tailwind css

--- src/Pages/Businesscard.jsx
+++ src/Pages/Businesscard.jsx
@@ -1,30 +1,33 @@
 import Form from "../Components/Form";
 import Template1 from "../Templates/Template1";
-import Template2 from "../Templates/Template2";
-import Template3 from "../Templates/Template3";
+// import Template2 from "../Templates/Template2";
+// import Template3 from "../Templates/Template3";
+// import Template4 from "../Templates/Template4";
+// import Template5 from "../Templates/Template5";
 import { UserDataContext } from "../Context/userdatacontext";
 import { useContext } from "react";
-import "../style/Pages/Businesscard.scss";
-import Template4 from "../Templates/Template4";
-import Template5 from "../Templates/Template5";
+
 const Businesscard = () => {
   const { userData } = useContext(UserDataContext);
 
   return (
-    <>
-      <div className="maindiv">
-        <div className="cards">
+    <div className="flex flex-col md:flex-row gap-6 p-4">
+      {/* Form Section */}
+      <div className="w-full md:w-1/3">
+        <Form />
+      </div>
+
+      {/* Cards Section */}
+      <div className="w-full md:w-2/3 overflow-y-auto max-h-[90vh] pr-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           <Template1 temp1Data={userData} />
-          <Template2 temp2Data={userData} />
+          {/* <Template2 temp2Data={userData} />
           <Template3 temp3Data={userData} />
           <Template4 temp4Data={userData} />
-          <Template5 temp5Data={userData} />
-        </div>
-        <div className="form">
-          <Form />
+          <Template5 temp5Data={userData} /> */}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/Templates/Template1.jsx
+++ src/Templates/Template1.jsx
@@ -1,91 +1,118 @@
 /* eslint-disable react/prop-types */
-import "../style/Templates/Template1.scss";
+import { useRef, useContext } from "react";
+import QRCode from "react-qr-code";
 import { HiOutlineMail } from "react-icons/hi";
 import { HiPhone, HiMiniGlobeAsiaAustralia } from "react-icons/hi2";
 import { CgWebsite } from "react-icons/cg";
 import { FaHouseChimney } from "react-icons/fa6";
-import QRCode from "react-qr-code";
 import DownloadButton from "../Components/DownloadButton";
-import { useRef } from "react";
 import { UserDataContext } from "../Context/userdatacontext";
-import { useContext } from "react";
 
-const Template = ({ temp1Data }) => {
+import "../style/Templates/Template1.scss"; // Import clip path styles
+
+const Template1 = ({ temp1Data }) => {
   const cardRef = useRef(null);
   const { vCardData } = useContext(UserDataContext);
 
   return (
     <>
-      <div ref={cardRef} className="template1">
-        <div className="flip-inner">
-          {/* Front Side */}
-          <div className="front-temp ">
-            <div className="temp1-bg"></div>
-            <div className="temp1-name">
-              <strong>
-                {temp1Data?.fname && temp1Data?.lname
-                  ? `${temp1Data.fname} ${temp1Data.lname}`
-                  : "Your Name"}
-              </strong>
-              <br />
-              <small>{temp1Data?.designation || "Designation"}</small>
-            </div>
-            <div className="temp1-content">
-              <div className="temp1-h">
-                <div style={{ backgroundColor: "red" }}>
-                  <HiOutlineMail />
+      {/* Wrapper to scale card responsively */}
+      <div className="w-max ">
+        <div
+          className="
+          transform origin-top-left
+          sm:scale-[1]
+          xs:scale-[0.8]
+          relative z-10
+          "
+          style={{ maxWidth: "370px" }}
+        >
+          <div
+            ref={cardRef}
+            className="relative rounded-lg overflow-hidden h-[220px] w-[370px] border border-black template1"
+          >
+            {/* Overflow hidden wrapper to keep contents inside */}
+            <div className="relative w-full h-full overflow-hidden">
+              {/* Front Side */}
+              <div className="absolute top-0 left-0 w-full h-full bg-white z-20 flex">
+                {/* Background shape with clip path */}
+                <div className="absolute w-full h-full -z-10 bg-black clip-template1-bg" />
+
+                {/* Name area with clip path */}
+                <div className="absolute top-[25px] left-0 w-[260px] h-[40px] text-white uppercase px-[20px] py-[5px] bg-red-600 text-xs clip-template1-name">
+                  <strong>
+                    {temp1Data?.fname && temp1Data?.lname
+                      ? `${temp1Data.fname} ${temp1Data.lname}`
+                      : "Your Name"}
+                  </strong>
+                  <br />
+                  <small>{temp1Data?.designation || "Designation"}</small>
                 </div>
-                &nbsp;
-                {temp1Data?.email || "username@example.com"}
-              </div>
-              <div className="temp1-h">
-                <div style={{ backgroundColor: "#b60b0b" }}>
-                  <HiPhone />
-                </div>
-                &nbsp;
-                {temp1Data?.countryCode && temp1Data?.phoneNumber
-                  ? `+${temp1Data.countryCode}-${temp1Data.phoneNumber}`
-                  : "+91-98xxxxxx98"}
-              </div>
-              <div className="temp1-h">
-                <div style={{ backgroundColor: "red" }}>
-                  <FaHouseChimney />
-                </div>
-                &nbsp;
-                {temp1Data.address || "address"}
-              </div>
-              {temp1Data?.website && (
-                <div className="temp1-h">
-                  <div style={{ backgroundColor: "#b60b0b" }}>
-                    <HiMiniGlobeAsiaAustralia />
+
+                {/* Contact and other info */}
+                <div className="absolute top-[93px] left-[20px] w-[250px] h-[90px] text-gray-500 text-xs space-y-1">
+                  <div className="flex items-center gap-1">
+                    <div className=" p-1 bg-red-600 text-white font-bold inline-block">
+                      <HiOutlineMail />
+                    </div>
+                    {temp1Data?.email || "username@example.com"}
                   </div>
-                  &nbsp;
-                  {temp1Data.website || "www.example.com"}
-                </div>
-              )}
-              {temp1Data?.gst && (
-                <div className="temp1-h">
-                  <div style={{ backgroundColor: "red" }}>
-                    <CgWebsite />
+
+                  <div className="flex items-center gap-1">
+                    <div className="p-1 bg-[#b60b0b] text-white font-bold inline-block">
+                      <HiPhone />
+                    </div>
+                    {temp1Data?.countryCode && temp1Data?.phoneNumber
+                      ? `+${temp1Data.countryCode}-${temp1Data.phoneNumber}`
+                      : "+91-98xxxxxx98"}
                   </div>
-                  &nbsp;
-                  {temp1Data.gst || "24AAACC1206D1ZT"}
+
+                  <div className="flex items-center gap-1">
+                    <div className=" p-1 bg-red-600 text-white font-bold inline-block">
+                      <FaHouseChimney />
+                    </div>
+                    {temp1Data?.address || "address"}
+                  </div>
+
+                  {temp1Data?.website && (
+                    <div className="flex items-center gap-1">
+                      <div className=" p-1 bg-[#b60b0b] text-white font-bold inline-block">
+                        <HiMiniGlobeAsiaAustralia />
+                      </div>
+                      {temp1Data.website}
+                    </div>
+                  )}
+
+                  {temp1Data?.gst && (
+                    <div className="flex items-center gap-1">
+                      <div className=" p-1 bg-red-600 text-white font-bold inline-block">
+                        <CgWebsite />
+                      </div>
+                      {temp1Data.gst}
+                    </div>
+                  )}
                 </div>
-              )}
+
+                {/* QR Code */}
+                <div className="absolute top-[90px] right-[20px] w-[88px] h-[88px] bg-white rounded-md p-1 flex items-center justify-center">
+                  <QRCode value={vCardData} size={80} />
+                </div>
+
+                {/* Decorations with clip paths */}
+                <div className="absolute top-[64.5px] left-[206px] w-[86px] h-[32px] bg-[#be1e1e] clip-template1-red-ts" />
+                <div className="absolute top-[75px] left-[93px] w-[200px] h-[150px] bg-[#be1e1e] clip-template1-red-tb" />
+              </div>
+
+              {/* Download button bottom right inside card */}
             </div>
-            <div id="temp1-qr">
-              <QRCode value={vCardData} size={80} />
-            </div>
-            <div className="red-ts"></div>
-            <div className="red-tb"></div>
           </div>
         </div>
-      </div>
-      <div className="template1-dbtn">
-        <DownloadButton targetRef={cardRef} fileName="CardTemplate" />
+        <div className="relative left-[300px] top-[-30px] z-50">
+          <DownloadButton targetRef={cardRef} fileName="CardTemplate" />
+        </div>
       </div>
     </>
   );
 };
 
-export default Template;
+export default Template1;
